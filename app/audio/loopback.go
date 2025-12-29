30d027bd982850b0f14439d75206e8e68684abca
torben conto <torbenmconto@gmail.com>
2025-12-29 18:02:54 -0500

    common interface

---
+++ app/audio/loopback.go
@@ -0,0 +1,12 @@
+package audio
+
+type Loopback interface {
+	// Initalize captue
+	Start() error
+
+	// Stop capture
+	Stop() error
+
+	// Next chunk of audio as f32 PCM
+	Read() ([]float32, error)
+}
