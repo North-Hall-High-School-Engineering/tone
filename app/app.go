a18b2744445600a0d1d7dc8390239d35deb84364
torben conto <torbenmconto@gmail.com>
2025-12-30 19:14:35 -0500

    remove junk

--- app/app.go
+++ app/app.go
@@ -3,8 +3,6 @@ package main
 import (
 	"app/audio"
 	"context"
-
-	"github.com/wailsapp/wails/v2/pkg/runtime"
 )
 
 type App struct {
@@ -25,57 +23,6 @@ func (a *App) startup(ctx context.Context) {
 	}
 
 	a.lo = lo
-	go func() {
-		for {
-			select {
-			case <-ctx.Done():
-				return
-			default:
-				samples, err := a.lo.Read()
-				if err != nil {
-					println("read error:", err.Error())
-					continue
-				}
-				if len(samples) > 0 {
-					// prepare debug info
-					debug := map[string]any{
-						"numSamples": len(samples),
-						"maxValue":   maxFloat32(samples),
-						"minValue":   minFloat32(samples),
-					}
-
-					// send to frontend
-					runtime.EventsEmit(a.ctx, "audioDebug", debug)
-				}
-			}
-		}
-	}()
-}
-
-func maxFloat32(s []float32) float32 {
-	if len(s) == 0 {
-		return 0
-	}
-	max := s[0]
-	for _, v := range s {
-		if v > max {
-			max = v
-		}
-	}
-	return max
-}
-
-func minFloat32(s []float32) float32 {
-	if len(s) == 0 {
-		return 0
-	}
-	min := s[0]
-	for _, v := range s {
-		if v < min {
-			min = v
-		}
-	}
-	return min
 }
 
 func (a *App) shutdown(ctx context.Context) {
