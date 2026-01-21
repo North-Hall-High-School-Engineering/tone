d3e27ec17db87211107c65b2f6aa208e02bf99f9
torben conto <torbenmconto@gmail.com>
2026-01-21 17:12:06 -0500

    naming, bugfixes, cleanup

--- model/train.py
+++ model/train.py
@@ -1,21 +1,26 @@
-import zipfile
-import urllib.request
 import glob
 import os
+import urllib.request
+import zipfile
+
 from datasets import Dataset
 
 # !wget -q https://zenodo.org/record/1188976/files/Audio_Speech_Actors_01-24.zip
 # !unzip -q Audio_Speech_Actors_01-24.zip -d ravdess
 
+
 def main():
+    ravdess_fname = "ravdess.zip"
+    if not os.path.exists(ravdess_fname):
+        urllib.request.urlretrieve(
+            "https://zenodo.org/record/1188976/files/Audio_Speech_Actors_01-24.zip",
+            ravdess_fname,
+        )
 
-    if not os.path.exists("RAVDESS.zip"):
-        urllib.request.urlretrieve("https://zenodo.org/record/1188976/files/Audio_Speech_Actors_01-24.zip", "RAVDESS.zip")
+        with zipfile.ZipFile(ravdess_fname, "r") as archive:
+            archive.extractall("ravdess")
 
-    with zipfile.ZipFile("RAVDESS.zip", "r") as RAVDESSfile:
-        RAVDESSfile.extractall("RAVDESS_DATA")
-
-        emotion_dict = {
+    emotion_dict = {
         "01": "neutral",
         "02": "calm",
         "03": "happy",
@@ -23,13 +28,15 @@ def main():
         "05": "angry",
         "06": "fearful",
         "07": "disgust",
-        "08": "surprised"
-    } # code -> label
+        "08": "surprised",
+    }  # code -> label
 
     emotions = list(emotion_dict.values())
     emotion2id = {e: i for i, e in enumerate(emotions)}
 
-    files = glob.glob("RAVDESS_DATA/**/*.wav") # collect all .wav files
+    files = glob.glob(
+        os.path.join(ravdess_fname, "/**/*.wav")
+    )  # collect all .wav files
 
     valid_paths = []
     labels = []
@@ -49,14 +56,11 @@ def main():
             print("invalid emotion, skipping", fname)
             continue
 
-
         valid_paths.append(path)
         labels.append(emotion2id[emotion])
 
-    dataset = Dataset.from_dict({
-        "audio": valid_paths,
-        "label": labels
-    })
+    dataset = Dataset.from_dict({"audio": valid_paths, "label": labels})
+
 
 if __name__ == "__main__":
     main()
