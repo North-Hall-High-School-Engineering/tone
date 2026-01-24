b1c041cf797b129f98af0a97a505464eee605f9f
Spencer Perry <Spencer.e.perry@outlook.com>
2026-01-24 00:48:03 -0500

    Factored out RAVDESS download into a new function

--- model/train.py
+++ model/train.py
@@ -4,21 +4,47 @@ import urllib.request
 import zipfile
 
 from datasets import Dataset
+from tqdm import tqdm
+
 
 # !wget -q https://zenodo.org/record/1188976/files/Audio_Speech_Actors_01-24.zip
 # !unzip -q Audio_Speech_Actors_01-24.zip -d ravdess
 
-
-def main():
+def load_ravdess_dataset():
     ravdess_fname = "ravdess.zip"
-    if not os.path.exists(ravdess_fname):
-        urllib.request.urlretrieve(
-            "https://zenodo.org/record/1188976/files/Audio_Speech_Actors_01-24.zip",
-            ravdess_fname,
-        )
+    try:
+        if not os.path.exists(ravdess_fname):
+            print("Ravdess dataset not found. Downloading...")
+
+            with tqdm(unit="B", unit_scale=True, miniters=1, desc="Progress: ") as t:
+                def reporthook(block_size, total_size):
+                    if t.total > 0:
+                        t.total = total_size
+                    t.update(block_size)
+                urllib.request.urlretrieve(
+                    "https://zenodo.org/record/1188976/files/Audio_Speech_Actors_01-24.zip",
+                    ravdess_fname,
+                    reporthook,
+                )
 
         with zipfile.ZipFile(ravdess_fname, "r") as archive:
-            archive.extractall("ravdess")
+            members = archive.namelist()
+
+            for member in tqdm(members, desc="Extracting RAVDESS dataset"):
+                archive.extract(member, "ravdess")    
+        print("RAVDESS dataset downloaded and extracted.")
+        return "ravdess"
+    
+    except Exception as e:
+        print("Error downloading or extracting RAVDESS dataset:", e)
+        return None
+
+
+def main():
+    ravdess_fname = load_ravdess_dataset()
+    if ravdess_fname is None:
+        print("Failed to load RAVDESS dataset.")
+        return
 
     emotion_dict = {
         "01": "neutral",
@@ -35,8 +61,9 @@ def main():
     emotion2id = {e: i for i, e in enumerate(emotions)}
 
     files = glob.glob(
-        os.path.join(ravdess_fname, "/**/*.wav")
-    )  # collect all .wav files
+    os.path.join(ravdess_fname, "**", "*.wav"),
+    recursive=True
+)
 
     valid_paths = []
     labels = []
@@ -60,6 +87,8 @@ def main():
         labels.append(emotion2id[emotion])
 
     dataset = Dataset.from_dict({"audio": valid_paths, "label": labels})
+    print(dataset)
+
 
 
 if __name__ == "__main__":
